6875955e4990507313f25c32fef0639727823fca
Shaik Anees Ahmad <115131511+Aneeshaik@users.noreply.github.com>
2024-08-22 20:57:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.post("/", function(req, res){
     https.get(url, function(response){
         response.on("data", function(d){
             const weatherData = JSON.parse(d);
-            // console.log(weatherData);
+            console.log(weatherData);
             const temp = weatherData.main.temp;
             const weatherCondition = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
